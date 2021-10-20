98136dfd9f53d07ad2ceb792d835fbba6925c8b5
Vinicius Andrade <viniandrd@hotmail.com>
2021-10-20 13:18:58 -0300

    Config initial and final slices

--- config.py
+++ config.py
@@ -1,13 +1,19 @@
 config = dict()
 
 # BraTS19 dataset path (extracted once)
-config['dataset_path'] = 'D:/ViniAndrade/TCC/ConjuntodeDados/BraTS19/training/'
+config['dataset_path'] = r'D:\Dados\ViniAndrade\TCC\BraTS19\MICCAI_BraTS_2019_Data_Training'
 
 # Path for images extraction
-config['images_path'] = 'D:/ViniAndrade/TCC/ConjuntodeDados/BraTS19 PNG 70-120/'
+config['images_path'] = r'D:\Dados\ViniAndrade\TCC\BraTS19\BraTS19 PNG\'
 
 # Which MR modality extract
 config['modality'] = 'flair'
+
+# Extract slices starting from
+config['initial_slice'] = 70
+
+# Extract slices until
+config['final_slice'] = 120
 
 # Input shape for segmentation_models
 config['input_shape'] = (None, None, 1)
